8985610335c85995badffed0f58ba103efb8ef0e
Michael Yang <mike@captain-refactor.co.za>
2013-05-23 21:16:54 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZurbMothershipApp::Application.config.secret_token = 'b7dd2e73fa2f9fbe5880f0d17b501dae4042cd144c3ce8ac3f963ee04b0919eefbae60c46f7e15ee11e8ec996ee1841db89c5e072ff8236059e07a9b4a217695'
